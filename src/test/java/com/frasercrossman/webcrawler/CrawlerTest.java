b03041705452603848f544482d51dfcbb62e32b6
Fraser Crossman <frasercrossman@outlook.com>
2019-08-25 17:56:25 +0100

    Added two tests to check only links with matching hostnames are visited

--- src/test/java/com/frasercrossman/webcrawler/CrawlerTest.java
+++ src/test/java/com/frasercrossman/webcrawler/CrawlerTest.java
@@ -1,5 +1,7 @@
 package com.frasercrossman.webcrawler;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.everyItem;
 import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -8,6 +10,7 @@ import java.net.URL;
 import java.util.Map;
 import java.util.Set;
 import org.junit.Test;
+import org.hamcrest.Matchers;
 
 public class CrawlerTest {
 
@@ -18,5 +21,27 @@ public class CrawlerTest {
     Map<URL, Set<URL>> sitemap = c.crawlSite(url);
 
     assertThat(sitemap.keySet(), hasItem(url));
+  }
+
+  @Test
+  public void testSitemapKeyContainNoForeignURLs() throws MalformedURLException {
+    Crawler c = new Crawler();
+    URL url = new URL("https://frasercrossman.com/");
+    Map<URL, Set<URL>> sitemap = c.crawlSite(url);
+
+    assertThat(sitemap.keySet(),
+        everyItem(Matchers.hasProperty("host", equalTo(url.getHost()))));
+  }
+
+  @Test
+  public void testSitemapValuesContainNoForeignURLs() throws MalformedURLException {
+    Crawler c = new Crawler();
+    URL url = new URL("https://frasercrossman.com/");
+    Map<URL, Set<URL>> sitemap = c.crawlSite(url);
+
+    sitemap.keySet().forEach(key -> {
+      assertThat(sitemap.get(key),
+          everyItem(Matchers.hasProperty("host", equalTo(url.getHost()))));
+    });
   }
 }
